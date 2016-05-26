d1643b379cd4fcb98ba6fd0b7b88463652f42d5b
Kyle Lacy <kylewlacy@me.com>
2016-05-25 19:39:30 -0700

    Use `objc!` macro for `IsNSApplicationDelegate`

--- src/protocols/ns_application_delegate.rs
+++ src/protocols/ns_application_delegate.rs
@@ -1,8 +1,8 @@
 use std::mem;
 use objc;
-use {objc_bool_to_rust, objc_to_rust, Object, AnyObject, ShareId,
-     IsNSObject, NSObject, NSApplication, NSNotification,
-     IntoObjC, ObjCInto, RawObjCObject};
+use objc::runtime as rt;
+use {Object, AnyObject, ShareId, IsNSObject, NSObject, NSApplication,
+     NSNotification, IntoObjC, ObjCInto, RawObjCObject};
 
 #[repr(usize)]
 pub enum NSApplicationActivationPolicy {
@@ -45,35 +45,6 @@ impl ObjCInto<NSApplicationTerminateReply> for usize {
 }
 
 
-
-#[derive(Default)]
-struct NSApplicationDelegateDefaultImpl;
-
-impl NSApplicationDelegateDefaultImpl {
-    fn application_will_finish_launching(&self, _notification: ShareId<NSNotification>) {
-
-    }
-
-    fn application_did_finish_launching(&self, _notification: ShareId<NSNotification>) {
-
-    }
-
-    fn application_should_terminate(&self, _sender: ShareId<NSApplication>)
-        -> NSApplicationTerminateReply
-    {
-        NSApplicationTerminateReply::TerminateNow
-    }
-
-    fn application_should_terminate_after_last_window_closed(&self, _sender: ShareId<NSApplication>)
-        -> bool
-    {
-        false
-    }
-
-    fn application_will_terminate(&self, _notification: ShareId<NSNotification>) {
-
-    }
-}
 
 #[repr(C)]
 pub struct NSApplicationDelegate {
@@ -122,115 +93,63 @@ pub trait IsNSApplicationDelegate: IsNSObject {
     }
 }
 
-impl IsNSApplicationDelegate for NSApplicationDelegate {
-    fn application_will_finish_launching(&self, notification: ShareId<NSNotification>) {
-        unsafe {
-            if objc_bool_to_rust(msg_send![self, respondsToSelector:sel!(applicationWillFinishLaunching:)]) {
-                let notification_ptr: *const NSNotification = &*notification;
-                let notification_ptr = notification_ptr as *const AnyObject;
-                msg_send![self, applicationWillFinishLaunching:notification_ptr];
-            }
-        }
+#[derive(Default)]
+struct NSApplicationDelegateDefaultImpl;
+
+impl NSApplicationDelegateDefaultImpl {
+    fn application_will_finish_launching(&self, _notification: ShareId<NSNotification>) {
+
     }
 
-    fn application_did_finish_launching(&self, notification: ShareId<NSNotification>) {
-        unsafe {
-            if objc_bool_to_rust(msg_send![self, respondsToSelector:sel!(applicationDidFinishLaunching:)]) {
-                let notification_ptr: *const NSNotification = &*notification;
-                let notification_ptr = notification_ptr as *const AnyObject;
-                msg_send![self, applicationDidFinishLaunching:notification_ptr];
-            }
-        }
+    fn application_did_finish_launching(&self, _notification: ShareId<NSNotification>) {
+
     }
 
-    fn application_should_terminate(&self, sender: ShareId<NSApplication>)
+    fn application_should_terminate(&self, _sender: ShareId<NSApplication>)
         -> NSApplicationTerminateReply
     {
-        unsafe {
-            if objc_bool_to_rust(msg_send![self, respondsToSelector:sel!(applicationShouldTerminate:)]) {
-                let sender_ptr: *const NSApplication = &*sender;
-                let sender_ptr = sender_ptr as *const AnyObject;
-                let application_should_terminate: usize = msg_send![self, applicationShouldTerminate:sender_ptr];
-                objc_to_rust(application_should_terminate)
-            }
-            else {
-                NSApplicationDelegateDefaultImpl.application_should_terminate(sender)
-            }
-        }
+        NSApplicationTerminateReply::TerminateNow
     }
 
-    fn application_should_terminate_after_last_window_closed(&self, sender: ShareId<NSApplication>)
+    fn application_should_terminate_after_last_window_closed(&self, _sender: ShareId<NSApplication>)
         -> bool
     {
-        unsafe {
-            if objc_bool_to_rust(msg_send![self, respondsToSelector:sel!(applicationShouldTerminate:)]) {
-                let sender_ptr: *const NSApplication = &*sender;
-                let sender_ptr = sender_ptr as *const AnyObject;
-                objc_bool_to_rust(msg_send![self, applicationShouldTerminate:sender_ptr])
-            }
-            else {
-                NSApplicationDelegateDefaultImpl.application_should_terminate_after_last_window_closed(sender)
-            }
-        }
+        false
     }
 
-    fn application_will_terminate(&self, notification: ShareId<NSNotification>) {
-        unsafe {
-            if objc_bool_to_rust(msg_send![self, respondsToSelector:sel!(applicationWillTerminate:)]) {
-                let notification_ptr: *const NSNotification = &*notification;
-                let notification_ptr = notification_ptr as *const AnyObject;
-                msg_send![self, applicationWillTerminate:notification_ptr];
-            }
-        }
+    fn application_will_terminate(&self, _notification: ShareId<NSNotification>) {
+
     }
 }
 
-impl<T> IsNSApplicationDelegate for T
-    where T: IsNSObject + SubNSApplicationDelegate
-{
-    default fn application_will_finish_launching(&self, notification: ShareId<NSNotification>)
-    {
-        self.super_ns_application_delegate_ref().application_will_finish_launching(notification);
-    }
+objc! {
+    pub unsafe objc trait IsNSApplicationDelegate: IsNSObject {
+        type Base = NSApplicationDelegate;
+        trait Sub = SubNSApplicationDelegate;
+        type DefaultImpl = NSApplicationDelegateDefaultImpl;
 
-    default fn application_did_finish_launching(&self, notification: ShareId<NSNotification>) {
-        self.super_ns_application_delegate_ref().application_did_finish_launching(notification)
-    }
+        #[optional]
+        fn application_will_finish_launching(&self, notification: ShareId<NSNotification>)
+            => [self, applicationWillFinishLaunching:(notification: *mut AnyObject)];
 
-    default fn application_should_terminate(&self, sender: ShareId<NSApplication>)
-        -> NSApplicationTerminateReply
-    {
-        self.super_ns_application_delegate_ref().application_should_terminate(sender)
-    }
+        #[optional]
+        fn application_did_finish_launching(&self, notification: ShareId<NSNotification>)
+            => [self, applicationDidFinishLaunching:(notification: *mut AnyObject)];
 
-    default fn application_should_terminate_after_last_window_closed(&self, sender: ShareId<NSApplication>)
-        -> bool
-    {
-        self.super_ns_application_delegate_ref().application_should_terminate_after_last_window_closed(sender)
-    }
+        #[optional]
+        fn application_should_terminate(&self, sender: ShareId<NSApplication>)
+            -> NSApplicationTerminateReply
+            => [self, applicationShouldTerminate:(sender: *mut AnyObject)]
+                -> usize;
 
-    default fn application_will_terminate(&self, notification: ShareId<NSNotification>) {
-        self.super_ns_application_delegate_ref().application_will_terminate(notification);
-    }
-}
+        #[optional]
+        fn application_should_terminate_after_last_window_closed(&self, sender: ShareId<NSApplication>)
+            -> bool
+            => [self, applicationShouldTerminateAfterLastWindowClosed:(sender: *mut AnyObject)]
+                -> rt::BOOL;
 
-pub trait SubNSApplicationDelegate {
-    type SuperNSApplicationDelegate: IsNSApplicationDelegate;
-
-    fn super_ns_application_delegate_ref(&self) -> &Self::SuperNSApplicationDelegate;
-    fn super_ns_application_delegate_mut(&mut self) -> &mut Self::SuperNSApplicationDelegate;
-}
-
-impl<T> SubNSApplicationDelegate for T
-    where T: Object, T::Super: IsNSApplicationDelegate
-{
-    type SuperNSApplicationDelegate = T::Super;
-
-    fn super_ns_application_delegate_ref(&self) -> &Self::SuperNSApplicationDelegate {
-        self.super_ref()
-    }
-
-    fn super_ns_application_delegate_mut(&mut self) -> &mut Self::SuperNSApplicationDelegate {
-        self.super_mut()
+        #[optional]
+        fn application_will_terminate(&self, notification: ShareId<NSNotification>)
+            => [self, applicationWillTerminate:(notification: *mut AnyObject)];
     }
 }
